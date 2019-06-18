fb82570234ea4b305d46dcb14cb5df44643a6ec8
Alvaro <alvaro_sgs16@hotmail.com>
2019-06-18 19:16:55 +0200

    cargarUsuarios

--- controladores/controladores.js
+++ controladores/controladores.js
@@ -58,49 +58,49 @@ function mapa(req, res) {
 }
 
 function update(req, res) {
-
+    
     var idDispositivo = req.body.dispositivoAleatorio
     var estadoDispositivoNuevo = req.body.estadoDispositivoAleatorio
-
+    
     Zona.updateOne({ "Dispositivos.idDispositivo": idDispositivo },
-        {
-            $set: {
-                "Dispositivos.$.estadoDispositivo": estadoDispositivoNuevo
-            }
-        }, { new: true }).exec((err, zonas) => {
-            if (err) {
-                res.status(500).send({
-                    message: "Error en el servidor",
-                    error: err
+    {
+        $set: {
+            "Dispositivos.$.estadoDispositivo": estadoDispositivoNuevo
+        }
+    }, { new: true }).exec((err, zonas) => {
+        if (err) {
+            res.status(500).send({
+                message: "Error en el servidor",
+                error: err
+            })
+        } else {
+            if (zonas != 0) {
+                
+                res.status(200).send({
+                    encontrado: true,
+                    zonas: zonas
                 })
             } else {
-                if (zonas != 0) {
-
-                    res.status(200).send({
-                        encontrado: true,
-                        zonas: zonas
-                    })
-                } else {
-                    res.status(200).send({
-                        encontrado: false
-                    })
-                }
+                res.status(200).send({
+                    encontrado: false
+                })
             }
-        })
+        }
+    })
 }
 
 function validar2(req, res) {
-
+    
     console.log("req.params");
     console.log(req.params);
     console.log("req.params.user");
     console.log(req.params.user);
-
+    
     var params = req.params;
-
+    
     var paramsUser = params.user;
     var paramsPass = params.pass;
-
+    
     Usuario.find({ "user": paramsUser, "pass": paramsPass }).exec((err, usuario) => {
         if (err) {
             res.status(500).send({
@@ -122,11 +122,36 @@ function validar2(req, res) {
     })
 }
 
+// Función para cargar los usuarios que hay
+function cargarUsuarios(req, res) {
+
+    Usuario.find({}).exec((err, usuarios) => {
+        if (err) {
+            res.status(500).send({
+                message: "Error en el servidor",
+                error: err
+            })
+        } else {
+            if (usuarios != 0) {
+                res.status(200).send({
+                    encontrado: true,
+                    usuarios: usuarios
+                })
+            } else {
+                res.status(200).send({
+                    encontrado: false
+                })
+            }
+        }
+    })
+}
+
 /* Exportamos las funciones para poder usarlas en routes */
 module.exports = {
     validar,
     mapa,
     update,
-    validar2
+    validar2,
+    cargarUsuarios
 }
 
